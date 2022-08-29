c06ffc597c5875957214b78533c11515cea22468
Justin Saborouh <rubikmanjustin@gmail.com>
2022-08-29 10:48:06 -0700

    commit tree 3555716400407589d907671d576d397e0258ac59

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10) + 1;
+
+let num3, num4;
+num3 = 9;
+num4 = 5;
+let mod = num3%num4;
+let max = Math.max(2,5,2,3,1,5,20);
